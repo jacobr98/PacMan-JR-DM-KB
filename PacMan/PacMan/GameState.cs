74ef7cc994c3e628ce6ab73952eed40b84fd06e3
Danny Manzano <el.flaco.172@gmail.com>
2017-02-23 19:12:07 -0500

    Started with the game state

---
+++ PacMan/PacMan/GameState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    class GameState
+    {
+
+        Pacman Pacman { get; }
+        GhostPack Ghostpack { get; }
+        Maze Maze { get; }
+        Pen Pen { get; }
+        ScoreAndLives Score { get; }
+
+        public static GameState Parse(string filecontent)
+        {
+            string[] stringLines = File.ReadAllLines(filecontent);
+        }
+
+    }
+}
