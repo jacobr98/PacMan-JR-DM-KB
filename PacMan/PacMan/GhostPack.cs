8566094ebe58acaec6525caa8697567b085808d2
1532483 <1532483@2F16-19>
2017-03-03 16:18:01 -0500

    Adding some classes template

---
+++ PacMan/PacMan/GhostPack.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    public class GhostPack : Ghost
+    {
+        List<Ghost> ghosts;
+
+        public GhostPack()
+        {
+            ghosts = new List<Ghost>();
+        }
+
+        public bool CheckCollideGhosts(Vector2 v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetGhosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScareGhost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Ghost g)
+        {
+            this.ghosts.Add(g);
+        }
+
+        
+    }
+}
---
+++ PacMan/TempTestApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TempTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = File.ReadAllLines("levels.csv");
+
+            
+        }
+    }
+
+    public class GameState
+    {
+
+        public Pacman Pacman { get; }
+        public GhostPack Ghostpack { get; }
+        public Maze Maze { get; }
+        public Pen Pen { get; }
+        public ScoreAndLives Score { get; }
+
+        public static GameState Parse(string filecontent)
+        {
+            string[] stringLines = File.ReadAllLines(filecontent);
+        }
+
+    }
+
+    public class Pacman
+    {
+        private GameState controller;
+        private Maze maze;
+
+        public Pacman(GameState controller)
+        {
+            this.controller = controller;
+            this.maze = this.controller.Maze;
+        }
+
+        public void Move(Direction dir)
+        {
+
+        }
+
+        public void CheckCollisions()
+        {
+
+        }
+    }
+
+    public class ScoreAndLives
+    {
+        public int Lives { get; set; }
+        public int Score { get; set; }
+
+        public ScoreAndLives(GameState game)
+        {
+            this.Lives = game.Score.Lives;
+            this.Score = game.Score.Score;
+            foreach (Ghost g in game.Ghostpack.ghosts)
+            {
+                g.PacmanDied += deadPacman;
+            }
+        }
+
+        public void deadPacman()
+        {
+            this.Lives--;
+        }
+
+        public void incrementScore(ICollidable collidable)
+        {
+            this.Score += collidable.Points;
+        }
+    }
+}
