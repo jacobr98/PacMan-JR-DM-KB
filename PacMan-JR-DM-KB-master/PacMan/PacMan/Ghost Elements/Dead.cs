5c6bab2627190983991bd90ea612df2606e343a7
Jacob <1537385@2F18-21>
2017-03-20 12:09:09 -0400

    Organized Buisness Logic into folders Added Dead State for when pacman eats ghost

---
+++ PacMan-JR-DM-KB-master/PacMan/PacMan/Ghost Elements/Dead.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan
+{
+    //State for when pacman eats a ghost
+    class Dead : IGhostState
+    {
+        private Pen pen;
+        private Maze maze;
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
